cfafe9a84287dc398c074602b229aed5d6a641a7
mohammed.mahboob1 <mohammed.mahboob1@hscic.gov.uk>
2024-01-02 17:34:53 +0000

    Added terraform aws infrastructure files.

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "2.70.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-state-bucket"
+    key    = "state/terraform_state.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
