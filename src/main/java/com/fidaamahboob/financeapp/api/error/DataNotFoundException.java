96ec9d21c46b3ed1f92e44ed16ba16cfd79a8a1b
fidaa-mahboob <fidaa_786@hotmail.com>
2024-10-20 16:05:30 +0100

    Improved error handling.

--- src/main/java/com/fidaamahboob/financeapp/api/error/DataNotFoundException.java
+++ src/main/java/com/fidaamahboob/financeapp/api/error/DataNotFoundException.java
@@ -1,9 +1,7 @@
 package com.fidaamahboob.financeapp.api.error;
 
 public class DataNotFoundException extends RuntimeException {
-    private static long serialVersionUID = 1L;
-
-    public DataNotFoundException(int id) {
-        super("Error! Finance Record  (id = " + id + ") Not Found");
+    public DataNotFoundException(String message) {
+        super(message);
     }
 }
