19d02ea069c9e6ad35ff83cc9ab5acf5c9cd4b7d
fidaa-mahboob <fidaa_786@hotmail.com>
2024-10-21 17:49:15 +0100

    Restructured API resources

--- src/test/java/com/fidaamahboob/financeapp/FinanceAppApplicationTests.java
+++ src/test/java/com/fidaamahboob/financeapp/FinanceAppApplicationTests.java
@@ -1,13 +1,54 @@
 package com.fidaamahboob.financeapp;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fidaamahboob.financeapp.api.controller.FinanceDataController;
+import com.fidaamahboob.financeapp.api.model.FinanceData;
+import com.fidaamahboob.financeapp.api.service.FinanceDataService;
 
 @SpringBootTest
 class FinanceAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+	@Mock
+	private FinanceDataService financeDataService;
+
+	@InjectMocks
+	private FinanceDataController financeDataController;
+
+	private FinanceData financeData;
+
+	
 
 }
